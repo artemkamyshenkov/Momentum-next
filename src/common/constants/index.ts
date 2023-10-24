5f0b8d162859b7a7dc8fc2d75572a6e4975bea28
Камышенков Артем Сергеевич <kamishenkov@innovasystem.pro>
2023-10-24 21:01:58 +0300

    add weather style

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,7 +1,7 @@
 export const PERSON_NAME = 'personName';
 export const PERSON_GOAL = 'personGoal';
 export const MOMENTUM_APP = 'momentumApp';
-
+export const DEFAULT_CITY = 'Moscow';
 export const MESSAGES = {
   DEFAULT_GOAL_TEXT: 'Моя главная цель на сегодня: ',
 };
--- src/components/Weather/ui/Weather.tsx
+++ src/components/Weather/ui/Weather.tsx
@@ -1,30 +1,58 @@
 'use client';
 
 import { weatherService } from '@/common/api/weatherService';
+import type { ChangeEvent } from 'react';
 import React, { useEffect, useState } from 'react';
 import cn from 'classnames';
 import type { WeatherData } from '@/common/types/weather';
+import { DEFAULT_CITY } from '@/common/constants';
+import { useDebounce } from '@/common/hooks/useDebounce';
 import styles from './Weather.module.scss';
 
+// TODO: в ls значение + LOADER
 export const Weather = () => {
   const [currentWeather, setCurrentWeather] = useState<WeatherData>();
+  const [city, setCity] = useState(DEFAULT_CITY);
+  const debouncedCity = useDebounce(city, 500);
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e?.target?.value;
+    setCity(value);
+  };
   useEffect(() => {
     const getWeatherData = async () => {
-      const weatherData = await weatherService.getWeatherByCity('Moscow');
+      const weatherData = await weatherService.getWeatherByCity(debouncedCity);
       setCurrentWeather(weatherData);
     };
     getWeatherData();
-  }, []);
+  }, [debouncedCity]);
+
   return (
-    <div>
-      <i
-        className={cn(
-          'weather-icon owf',
-          `owf-${currentWeather?.weather[0].id}`,
-          styles.icon,
-        )}
-      />
-    </div>
+    currentWeather && (
+      <div className={styles.weatherContainer}>
+        <input
+          type="text"
+          className={styles.inputWeather}
+          onChange={handleChange}
+        />
+        <i
+          className={cn(
+            'weather-icon owf',
+            `owf-${currentWeather?.weather[0].id}`,
+            styles.icon,
+          )}
+        />
+        <div>
+          <span className={styles.temp}>
+            {Math.floor(currentWeather?.main?.temp || 0)}°C
+          </span>
+          <span>{currentWeather?.weather[0].description}</span>
+        </div>
+        <div>
+          Скорость ветра: {Math.floor(currentWeather?.wind?.speed || 0)} м/с
+        </div>
+        <div>Влажность: {Math.floor(currentWeather.main.humidity)}%</div>
+      </div>
+    )
   );
 };
