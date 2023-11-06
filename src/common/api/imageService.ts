65e054f64c39d1db89e37d47e92354b99b239437
Камышенков Артем Сергеевич <kamishenkov@innovasystem.pro>
2023-11-06 14:01:48 +0300

    add new photo api

--- src/common/api/imageService.ts
+++ src/common/api/imageService.ts
@@ -1,3 +1,4 @@
+import type { AxiosError } from 'axios';
 import axios from 'axios';
 import { FLICKR_API_KEY } from '../config';
 
@@ -5,13 +6,28 @@ const imageInstance = axios.create({
   baseURL: 'https://www.flickr.com/services/rest/',
 });
 export const imageService = {
-  getRandomImage: (): Promise<any> =>
+  getRandomImage: (tag?: string): Promise<string> =>
     imageInstance
       .get(
-        `?method=flickr.photos.search&api_key=${FLICKR_API_KEY}&tags=city&extras=url_l&format=json&nojsoncallback=1&per_page=1&page=${
+        `?method=flickr.photos.search&api_key=${FLICKR_API_KEY}&tags=${
+          tag || 'city'
+        }&extras=url_o&format=json&nojsoncallback=1&per_page=1&page=${
           Math.floor(Math.random() * 100) + 1
         }`,
       )
-      .then(res => res.data.photos)
-      .catch(e => Promise.reject(new Error(e))),
+      .then(res => {
+        const imgId = res.data.photos?.photo?.[0].id;
+
+        return imageInstance.get(
+          `?method=flickr.photos.getSizes&api_key=${FLICKR_API_KEY}&photo_id=${imgId}&format=json&nojsoncallback=1`,
+        );
+      })
+      .then(res => {
+        const sizes: Array<any> = res?.data.sizes.size;
+        const photo = sizes.findLast(
+          img => img.height >= 1000 && img.height <= 3000,
+        );
+        return photo?.source;
+      })
+      .catch((e: AxiosError) => Promise.reject(e)),
 };
--- src/components/Slider/ui/Slider.tsx
+++ src/components/Slider/ui/Slider.tsx
@@ -1,19 +1,38 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { imageService } from '@/common/api/imageService';
 import styles from './Slider.module.scss';
-
+/* 
+TODO: 
+1. Индикатор загрузки нового фото чтобы не появлялост дефолное
+2. Сохранить текущее фото и предыдущее
+*/
 export const Slider = () => {
+  const [isNextButtonDisabled, setNextButtonDisabled] = useState(false);
+  let nextButtonTimer: ReturnType<typeof setTimeout>;
   const handleClickNext = async () => {
     try {
+      setNextButtonDisabled(true);
       const res = await imageService.getRandomImage();
-      document.body.style.background = `url(${res.photo[0].url_l}) center/cover, rgba(0, 0, 0, 0.2)`;
+      document.body.style.background = `url(${res}) center/cover, rgba(0, 0, 0, 0.2)`;
     } catch (error) {
       console.error(error);
+    } finally {
+      nextButtonTimer = setTimeout(() => {
+        setNextButtonDisabled(false);
+      }, 5000);
     }
   };
   const handleClickPrev = () => console.log('prev');
+
+  useEffect(
+    () => () => {
+      clearTimeout(nextButtonTimer);
+    },
+    [],
+  );
+
   return (
     <>
       <button
@@ -25,6 +44,7 @@ export const Slider = () => {
         type="button"
         className={styles.slideNext}
         onClick={handleClickNext}
+        disabled={isNextButtonDisabled}
       />
     </>
   );
